9e81f1a2af365064f4c20a6f894ae8a7a86f3b50
Alejandro <alexmarco18795@gmail.com>
2019-06-06 15:40:40 +0000

    add mongo and basic funcionalities

--- app.js
+++ app.js
@@ -1,63 +1,114 @@
 var express = require("express"); //Call Express
 var app = express(); 
 var bodyParser= require("body-parser"); //Call Body_parse
-var mongoose = require("mongoose"); //Call Mongoose
+//var model = require("./model.js");
 
-app.use(bodyParser.urlencoded({extended:true})); //use body parser
+app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine", "ejs"); //Set ejs as a default view engine
 
+// MODEL
+
+var mongoose = require("mongoose"); //Call Mongoose
+
+//helpers
+var img1="https://images.unsplash.com/photo-1482222728531-f17c41146dd6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2657&q=80";
+var img2="https://images.unsplash.com/photo-1466201276674-c06f9bf24323?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80";
+var img3="https://images.unsplash.com/photo-1519109369405-acc801b7cf80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1951&q=80";
+
 //db
-mongoose.connect("mongodb://localhost/bike_app"); //Connect to database or create it
+mongoose.connect("mongodb://localhost/bike_app", { useNewUrlParser: true }); //Connect to database or create it
 
 //db schema
 var locationSchema = new mongoose.Schema({
     name: String,
-    img: String,
+    type: String,
+    spaces: Number,
+    geo:{
+        latitude: Number,
+        longitude: Number,
+    },
+    image: String,
+    cctv: Boolean,
     security: Number
-})
+});
 
 //take the previous pattern and we put it into a model
 var Location = mongoose.model("Location", locationSchema);
 
-//Variables
-  var places = [
-            { name: "South Bank", image: "https://images.unsplash.com/photo-1532522593358-9e5e2a22f231?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2930&q=80"},
-            { name: "Tower Bridge", image: "https://images.unsplash.com/photo-1522961881734-7df1e2635d0c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1567&q=80"},
-            { name: "Liverpool Street", image: "https://images.unsplash.com/photo-1508717765549-dee93e0044fa?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"}
-    ];
+//Adding a new location
+function addLocation(name,image) {
+
+    var newLocation = new Location({
+        //Generate name
+        name: name,
+        type: "other",
+        spaces: 6,
+        geo: {latitude: 0, longitude: 0},
+        image: image,
+        cctv: false,
+        security: 0
+    });
+    
+    newLocation.save(function(err,location){
+        if(err){
+            console.log("Error adding item to db");
+        }else{
+            console.log("New location added: " + location);
+        }
+    });
+}
+
+////////
+
 
 //Landing Page
 app.get("/", function(req,res){
     res.render("landing");
 });
 
-//List of places
-app.get("/places", function(req,res){
-    res.render("places", {places:places});
+//INDEX
+app.get("/locations", function(req,res){
+    
+    Location.find({}, function(err,locations){
+        if(err){
+            console.log("Unexpected error: " + err);
+        }else{
+            console.log("Retreiving locations...");
+            res.render("locations", {locations:locations});
+        }
+    });
 });
 
-app.post("/places", function(req,res){
+//CREATE 
+app.post("/locations", function(req,res){
     
     //Logic
     //Get data from fields in the Post request
     var name = req.body.name;
     var image = req.body.image;
     //Create a new object with data
-    var newPlace = {name:name, image:image};
-    //Add data to our array
-    places.push(newPlace);
-    
-    //Redirect to the places page
-    res.redirect("/places");
-    
+    //Add to database
+    addLocation(name,image);
+    //Redirect to the locations page
+    res.redirect("/locations");
     
 });
 
-//Form to add new place
-app.get("/places/new", function(req, res) {
+//NEW - Form to add new campground (must be before show route)
+app.get("/locations/new", function(req, res) {
    res.render("new"); 
 });
 
+//SHOW
+app.get("locations/:id", function(req, res) {
+    res.send("Future show route");
+});
+
 app.listen(process.env.PORT, process.env.IP, function(){
-    console.log("Server started... ")
+    console.log("Server started... ");
 }); 
+
+//Other Functions
+
+//Create name
+
--- model.js
+++ model.js
@@ -1,12 +1,24 @@
 var mongoose = require("mongoose"); //Call Mongoose
 
+//helpers
+var img1="https://images.unsplash.com/photo-1482222728531-f17c41146dd6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2657&q=80";
+var img2="https://images.unsplash.com/photo-1466201276674-c06f9bf24323?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80";
+var img3="https://images.unsplash.com/photo-1519109369405-acc801b7cf80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1951&q=80";
+
 //db
 mongoose.connect("mongodb://localhost/bike_app", { useNewUrlParser: true }); //Connect to database or create it
 
 //db schema
 var locationSchema = new mongoose.Schema({
     name: String,
-    img: String,
+    type: String,
+    spaces: Number,
+    geo:{
+        latitude: Number,
+        longitude: Number,
+    },
+    image: String,
+    cctv: Boolean,
     security: Number
 });
 
@@ -14,17 +26,60 @@ var locationSchema = new mongoose.Schema({
 var Location = mongoose.model("Location", locationSchema);
 
 //Adding a new location
-var newLocation = new Location({
-    name: "Tower Bridge",
-    img: "https://images.unsplash.com/photo-1522961881734-7df1e2635d0c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1567&q=80",
-    security: 5
-});
+function addLocation(type,spaces,image,cctv) {
 
-newLocation.save(function(err,location){
-    if(err){
-        console.log("Error adding item to dd");
+    var newLocation = new Location({
+        //Generate name
+        name: createName(type,spaces),
+        type: type,
+        spaces: spaces,
+        geo: {latitude: 0, longitude: 0},
+        image: image,
+        cctv: cctv,
+        security: 0
+    });
+    
+    newLocation.save(function(err,location){
+        if(err){
+            console.log("Error adding item to db");
+        }else{
+            console.log("New location added: " + location);
+        }
+    });
+}
+
+//Retreiving all locations
+function getLocations() {
+    
+    Location.find({}, function(err,locations){
+        if(err){
+            console.log("Unexpected error: " + err);
+        }else{
+            console.log("Retreiving locations...");
+        }
+    });
+}
+
+//Other Functions
+
+//Create name
+function createName(type,spaces) {
+    var size;
+    
+    if(spaces<=4){
+        size = "Small ";
+    }else if(spaces>=5 && spaces<=10){
+        size= "Medium ";
     }else{
-        console.log("New location added: " + location);
+        size= "Large ";
     }
-});
+    
+    return(size + type + " at London");
+}
+
+addLocation("deck", 20, img1, false);
+addLocation("wall hoop", 1, img3, false);
+addLocation("sheffield", 8, img2, true);
+
+
 
